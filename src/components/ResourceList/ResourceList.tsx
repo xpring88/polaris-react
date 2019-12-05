eccf00d3225df2005add54f8f19997e1cfba607d
Chloe Rice <chloe.rice@shopify.com>
2019-12-04 22:58:02 -0500

    Revert "Revert "Merge pull request #2160 from Shopify/resourcelist-emptystate""
    This reverts commit 6ec053f8dd30a227e29989e2baf7c9a5020812d1.

--- src/components/ResourceList/ResourceList.tsx
+++ src/components/ResourceList/ResourceList.tsx
@@ -51,6 +51,8 @@ export interface ResourceListProps {
   /** Item data; each item is passed to renderItem */
   items: Items;
   filterControl?: React.ReactNode;
+  /** The markup to render when no resources exist yet */
+  emptyState?: React.ReactNode;
   /** Name of the resource, such as customers or products */
   resourceName?: {
     singular: string;
@@ -381,7 +383,9 @@ class ResourceList extends React.Component<CombinedProps, State> {
       promotedBulkActions,
       bulkActions,
       filterControl,
+      emptyState,
       loading,
+      hasMoreItems,
       showHeader = false,
       sortOptions,
       sortValue,
@@ -474,9 +478,14 @@ class ResourceList extends React.Component<CombinedProps, State> {
       <div className={styles['HeaderWrapper-overlay']} />
     ) : null;
 
-    const showEmptyState = filterControl && !this.itemsExist() && !loading;
+    const showNoResults =
+      filterControl && !this.itemsExist() && hasMoreItems && !loading;
 
-    const headerMarkup = !showEmptyState &&
+    const showEmptyState =
+      emptyState !== undefined && !this.itemsExist() && !hasMoreItems;
+
+    const headerMarkup = !showNoResults &&
+      !showEmptyState &&
       (showHeader || needsHeader) &&
       this.listRef.current && (
         <div className={styles.HeaderOuterWrapper}>
@@ -515,11 +524,13 @@ class ResourceList extends React.Component<CombinedProps, State> {
         </div>
       );
 
-    const emptyStateMarkup = showEmptyState ? (
+    const noResultsMarkup = showNoResults ? (
       <div className={styles.EmptySearchResultWrapper}>
         <EmptySearchResult {...this.emptySearchResultText()} withIllustration />
       </div>
     ) : null;
+
+    const emptyStateMarkup = showEmptyState ? emptyState : null;
 
     const defaultTopPadding = 8;
     const topPadding =
@@ -564,9 +575,7 @@ class ResourceList extends React.Component<CombinedProps, State> {
         {loadingOverlay}
         {items.map(this.renderItem)}
       </ul>
-    ) : (
-      emptyStateMarkup
-    );
+    ) : null;
 
     const context = {
       selectable: this.selectable(),
@@ -584,6 +593,8 @@ class ResourceList extends React.Component<CombinedProps, State> {
           {filterControlMarkup}
           {headerMarkup}
           {listMarkup}
+          {noResultsMarkup}
+          {emptyStateMarkup}
           {loadingWithoutItemsMarkup}
         </div>
       </ResourceListContext.Provider>
